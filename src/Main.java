7be777bb827cdc7affcd056b5a7eae20ee711de1
DraxanLT <draxan.launay-tran@edu.esiee-it.fr>
2024-10-09 10:28:39 +0200

    Edit var name

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-import java.util.Random;  
+import java.util.Random;
 
 public class Main {
 
