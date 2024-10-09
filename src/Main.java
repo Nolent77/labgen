1c8b1e8a25e43e7d52deced7e612ae2c33d21573
Mine Map <draxan.launay-tran@edu.esiee-it.fr>
2024-10-09 20:21:54 +0200

    save before editing

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,30 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class Main {
 
+    static int line = 11;
+    static int column = 31;
+
     public static int randomValue() {
         Random rand = new Random();
-        return rand.nextInt(10) + 1;
+        return rand.nextInt(6) + 1;
+    }
+
+    public void gen(int[][] lab, int x, int y, boolean[][] visited) {
+        lab[x][y] = ' ';
+
+        int[] dx = {0, -1, 0, 1};
+        int[] dy = {1, 0, -1, 0};
+        Random random = new Random();
+        int direction = random.nextInt(4);
+        int posDirX = (dx[direction] * 2) + x;
+        int posDirY = (dy[direction] * 2) + y;
+        if (posDirX > 0 && posDirY > 0 && posDirX < line - 1 && posDirY < column - 1 && !visited[posDirX][posDirY]) {
+            visited[posDirX][posDirY] = true;
+            lab[posDirX][posDirY] = ' ';
+            gen(lab, posDirX, posDirY, visited);
+        }
     }
 
     public static String[][] mazeGenerator(int line, int column) {
@@ -14,21 +34,29 @@ public class Main {
         String[][] tab = new String[line][column];
         for (int i = 0; i < line; i++) {
             for (int j = 0; j < column; j++) {
-                if (i == 0 || i == line - 1 || j == 0 || j == column - 1) {
-                    tab[i][j] = "#";
-                } else {
-                    tab[i][j] = (aleaM.nextInt(4) == 0) ? "#" : " ";
+//                tab[i][j] = "##";
+                // ---- Generate grid empty spaces ----
+                if (i > 0 && j > 0 && i < line - 1 && j < column - 1) {
+                    tab[i][j] = "  ";
+                }
+                // ---- Generate grid ----
+                if (i % 2 == 0 || j % 2 == 0) {
+                    tab[i][j] = "##";
+                }
+                // ---- Generate border ----
+                if (i == 0 || j == 0 || i == line - 1 || j == column - 1) {
+                    tab[i][j] = "<>";
                 }
             }
         }
         for (int i = 0; i < line; i++) {
             for (int j = 0; j < column; j++) {
-                if ( alreadyExist) {
-                    tab[0][randomValue()] = "E";
+                if (alreadyExist) {
+                    tab[0][randomValue()] = "EE";
                     alreadyExist = false;
                 }
                 if (existingExist) {
-                    tab[line - 1][randomValue()] = "S";
+                    tab[line - 1][randomValue()] = "SS";
                     existingExist = false;
                 }
                 System.out.print(tab[i][j]);
@@ -38,11 +66,11 @@ public class Main {
         return tab;
     }
 
+    public static void main(String[] args) {
+        System.out.println(mazeGenerator(line, column));
 
-        public static void main (String[]args){
-            System.out.println(mazeGenerator(10, 30));
 
 
-        }
     }
 
+}
