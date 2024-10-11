7b52e44654d6097d8a801aa21af2cce9335b7926
ghazi <ghazi.khiari@edu.esiee-it.fr>
2024-10-11 14:23:00 +0200

    add mag at coms

--- src/Move.java
+++ src/Move.java
@@ -21,7 +21,7 @@ public class Move {
         int x = 1;
         int y = 1;
         boolean End = false;
-        //mettre le joueur sous E
+        //Mettre le joueur sous E
         for (int i = 0; i < GiveTab[0].length; i++) {
             if (GiveTab[0][i]=="E") {
                 x = 1;
@@ -29,22 +29,23 @@ public class Move {
             }
 
         }
-        //placer le joueur
+        //Placer le joueur
         GiveTab[x][y]="\uD83D\uDE21";
+        //Jusqu'a que sa fini
         while (!End){
-            //afficher le laby
+            //Afficher le laby
             Main.showMaze(GiveTab);
-            //recuperer les touche
+            //Recuperer les touche
             Scanner sc = new Scanner(System.in);
 
             String move = sc.nextLine();
-            //appeler le solver
+            //Appeler le solver
             if (move.equals("Ghazi and Liam the bg")){
                 GiveTab[x][y]=" ";
                 AutoSolver.pathSolver(GiveTab);
                 End = true;
             }
-                //les mouvement ZQSD avec un print pour dire qu'il y a un mure
+                //Les mouvement ZQSD avec un print pour dire qu'il y a un mure
             if (move.equals("d") ||move.equals("D")) {
                 if (GiveTab [x][y+1]=="#"){
                     GiveTab[x][y]="\uD83D\uDE21";
@@ -78,7 +79,7 @@ public class Move {
                     x-=1;
                 }
             }
-            //pour finir la boucle quand on est sur le S
+            //Pour finir la boucle quand on est sur le S
             if (move.equals("s") ||move.equals("S")) {
                 if (GiveTab [x+1][y]=="#"){
                     GiveTab[x][y]="\uD83D\uDE21";
