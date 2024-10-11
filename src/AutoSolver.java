58221902a9d5b4a2d78bb03faebe5fbd07f97044
Deparfouru Liam <liam.deparfouru@edu.esiee-it.fr>
2024-10-11 14:24:30 +0200

    Autosolver commentaires

--- src/AutoSolver.java
+++ src/AutoSolver.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 
 public class AutoSolver {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 
 //        String tableau[][]=
 //                        {       {"#","E","#","#","#","#","#","#","#","#"},
@@ -18,154 +18,98 @@ public class AutoSolver {
 ////        tableau = mazeGenerator(10, 30);
 //        chemin(tableau);
 
-    }
-    static boolean realisable(String[][] tab){
-
-        int x=0;
-        int y=0;
-        boolean fin=false;
-        boolean realisable=true;
-
-        for (int i = 0; i < tab[0].length; i++) {
-            if (tab[0][i]=="E"){
-                x=i;
-                y=1;
-            }
-        }
-
-        while (!fin && tab[x][y] != "S") {
-
-            //System.out.println(tab[x][y]);
-
-            if (tab[x][y + 1] == " " || tab[x][y + 1] == "S") {
-                tab[x][y] = "V";
-                y += 1;
-            }
-            else if (tab[x + 1][y] == " " || tab[x + 1][y] == "S") {
-                tab[x][y] = "V";
-                x += 1;
-            }
-            else if (tab[x][y - 1] == " " || tab[x][y - 1] == "S") {
-                tab[x][y] = "V";
-                y -= 1;
-            }
-            else if (tab[x - 1][y] == " " || tab[x - 1][y] == "S") {
-                tab[x][y] = "V";
-                x -= 1;
-            }
-            else if (tab[x][y + 1] == "V") {
-                tab[x][y] = "C";
-                y += 1;
-            }
-            else if (tab[x + 1][y] == "V") {
-                tab[x][y] = "C";
-                x += 1;
-            }
-            else if (tab[x][y - 1] == "V") {
-                tab[x][y] = "C";
-                y -= 1;
-            }
-            else if (tab[x - 1][y] == "V") {
-                tab[x][y] = "C";
-                x -= 1;
-            }
-            else {
-                realisable=false;
-                fin = true;
-            }
-        }
-        return realisable;
-    }
-
-
-    static void affiche(String[][] tab){
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab[i].length; j++) {
-                if (tab[i][j]=="\uD83d\uDE21"){
-                    System.out.print(tab[i][j]+"");
-                }
-                else {
-                    System.out.print(tab[i][j]+" ");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-
-    static void pathSolver(String[][] tab){
+    //Fonction qui récupère en argument un tableau et qui renvoie la solution à celui-ci
+    static void chemin(String[][] tab){
 
         int x=1;
         int y=1;
         boolean fin=false;
         boolean realisable=true;
 
+        //Affecte la position de départ à la sace en dessous de l'entrée
         for (int i = 0; i < tab[0].length; i++) {
             if (tab[0][i]=="E"){
                 x=1;
                 y=i;
             }
         }
+
+        //début du timer
         long start = System.nanoTime();
         int loops=0;
+        int comparaisons=0;
 
+        //Boucle qui cherche autour de la case actuelle et agit en conséquence
         while (!fin && tab[x][y] != "S") {
 
-//            System.out.println(x+"+"+y);
 
+            //Ces 4 if cherchent des cases jamais visitées
             if (tab[x + 1][y] == " " || tab[x + 1][y] == "S") {
                 tab[x][y] = "V";
                 x += 1;
+                comparaisons+=1;
             }
 
             else if (tab[x][y + 1] == " " || tab[x][y + 1] == "S") {
                 tab[x][y] = "V";
                 y += 1;
+                comparaisons+=2;
             }
 
             else if (tab[x - 1][y] == " " || tab[x - 1][y] == "S") {
                 tab[x][y] = "V";
                 x -= 1;
+                comparaisons+=3;
             }
 
             else if (tab[x][y - 1] == " " || tab[x][y - 1] == "S") {
                 tab[x][y] = "V";
                 y -= 1;
+                comparaisons+=4;
             }
 
+
+            //Ces 4 fonctions cherchent des cases déjà visitées si aucune case alentoure est non visitée
             else if (tab[x + 1][y] == "V") {
                 tab[x][y] = "C";
                 x += 1;
+                comparaisons+=5;
             }
 
             else if (tab[x][y + 1] == "V") {
                 tab[x][y] = "C";
                 y += 1;
+                comparaisons+=6;
             }
 
             else if (tab[x - 1][y] == "V") {
                 tab[x][y] = "C";
                 x -= 1;
+                comparaisons+=7;
             }
 
             else if (tab[x][y - 1] == "V") {
                 tab[x][y] = "C";
                 y -= 1;
+                comparaisons+=8;
             }
 
+
+            //Si il n'y a plus de chemins disponibles, le code s'arrête et le labyrinthe n'est pas réalisable
             else {
                 realisable=false;
                 fin = true;
+                comparaisons+=9;
             }
             loops++;
         }
-        long end = System.currentTimeMillis();
-        if (end-start > 0){
-            System.out.println("The Solver finished the maze in "+((end-start))+" milliseconds");
-        } else { System.out.println("The Solver finished the maze in "+((end-start))+" millisecond"); }
+        //Fin du timer
+        long end = System.nanoTime();
+        //Message si non réalisable
         if (!realisable){
             System.out.println("Non réalisable");
         }
+        //Modification du tableau et affichage si réalisable
         else{
             for (int i = 1; i < tab.length-1; i++) {
                 for (int j = 1; j < tab[i].length-1; j++) {
@@ -176,7 +120,8 @@ public class AutoSolver {
             }
             Main.showMaze(tab);
         }
-        System.out.println("Le solver s'est terminé en "+((end-start)/10000000.0)+" millisecondes et a effectué "+loops+" boucles.");
+        //Affichage de la complexité
+        System.out.println("Le solver s'est terminé en "+((end-start)/10000000.0)+" millisecondes et a effectué "+loops+" boucles et "+comparaisons+" comparaisons.");
 
     }
 }
